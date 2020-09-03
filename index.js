970aec5c656168623da7f5137f15ab5bd3e0ac8a
Ayesha <ayeshakhtar24@gmail.com>
2020-09-03 16:11:45 +0100

    navbar colour change across website

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // index js
+$(document).ready(function(){
+   var about_top = $(".about-section").offset().top;
+   var about_bottom = about_top + $(".about-section").outerHeight(true);
+   $(document).scroll(function() {
+      $(".social-icon").each(function() {
+        var icon_top = $(this).offset().top;
+        var icon_bottom = icon_top + $(this).outerHeight(true);
+        if (about_top <= icon_bottom && icon_bottom <= about_bottom) {
+            $(this).css('color', 'white');
+         } else {
+            $(this).css('color', '#a915cb');
+         }
+      });
+   });
+
+});
