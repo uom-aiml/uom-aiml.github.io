2fd397cbdb2f70bc5af648e85862be2588f34575
Ayesha <ayeshakhtar24@gmail.com>
2020-09-04 15:19:19 +0100

    adjusted index js

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ window.scrollTo(0,0);
 
 
 $(document).ready(function(){
+  $(".social-icon").hover(function() {
+    $(this).addClass("social-icon-hover");
+  }, function() {
+    $(this).removeClass("social-icon-hover");
+  });
+
   $(document).scroll(function() {
     var about_top = $(".about-section").offset().top;
     var about_bottom = about_top + $(".about-section").outerHeight(true);
@@ -21,27 +27,33 @@ $(document).ready(function(){
       var icon_top = $(this).offset().top;
       var icon_bottom = icon_top + $(this).outerHeight(true);
       if (about_top <= icon_bottom && icon_bottom <= about_bottom) {
-        $(this).css('color', 'white');
-        var icon_type = "white";
-        if (icon_type == "white"){
-          $(this).hover(function(){
-            $(this).css("color", "#5fcad8");
-            }, function(){
-            $(this).css("color", "white");
-          });
-        }
+        $(this).css("color", "white");
+      } else {
+        $(this).css("color", "#a915cb");
       }
-      else {
-        $(this).css('color', '#a915cb');
-        var icon_type = "purple";
-        if (icon_type == "purple"){
-          $(this).hover(function(){
-            $(this).css("color", "#5fcad8");
-            }, function(){
-            $(this).css("color", "#a915cb");
-          });
-        }
-      }
+
+      // if (about_top <= icon_bottom && icon_bottom <= about_bottom) {
+      //   $(this).css('color', 'white');
+      //   var icon_type = "white";
+      //   if (icon_type == "white"){
+      //     $(this).hover(function(){
+      //       $(this).css("color", "#5fcad8");
+      //       }, function(){
+      //       $(this).css("color", "white");
+      //     });
+      //   }
+      // }
+      // else {
+      //   $(this).css('color', '#a915cb');
+      //   var icon_type = "purple";
+      //   if (icon_type == "purple"){
+      //     $(this).hover(function(){
+      //       $(this).css("color", "#5fcad8");
+      //       }, function(){
+      //       $(this).css("color", "#a915cb");
+      //     });
+      //   }
+      // }
     });
   });
 });
